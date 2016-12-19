4de5cfd11c270355be9e90c2f902d17d03384f82
Malay Shukla <mshukla@sapient.com>
2016-12-19 11:41:51 +0000

    changes to allow resource files to all

--- src/main/java/com/udtamns/Application.java
+++ src/main/java/com/udtamns/Application.java
@@ -2,26 +2,17 @@
 package com.udtamns;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.udtamns.model.Users;
 import com.udtamns.repository.UserRepository;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner{
+public class Application {
 	
 	@Autowired
 	UserRepository user;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Override
-	public void run(String... arg0) throws Exception {
-		user.deleteAll();
-		user.save(new Users("Admin", "Admin","adminuser","killerapp123"));
-		
 	}
 }
